925ff225a6a6dd0432b55362ba3825885000de54
SebastianCorrea001 <darioalfonso125@gmail.com>
2026-09-03 15:29:36 -0500

    feat: funcion dar_pista al codigo

--- juego.py
+++ juego.py
@@ -11,28 +11,30 @@ def mostrar_bienvenida():
 def pedir_intento():
     return int(input("Introduce tu intento (entre 1 y 100): "))
 
+def dar_pista(intento, numero_secreto):
+    if intento < numero_secreto:
+        print("El número secreto es mayor. Intenta de nuevo.")
+    else:
+        print("El número secreto es menor. Intenta de nuevo.")
 
 def jugar():
     numero_secreto = random.randint(1, 100)
     intentos = 0
     max_intentos = 12
-    print("¡Bienvenido al juego de adivina el numero: !")
+    mostrar_bienvenida()
     print("Elige un numero entre 1 y 100.")
 
-    while True:
-        intento = int(input("Introduce tu intento: "))
+    while intentos < max_intentos:
+        intento = pedir_intento()
         intentos += 1
 
         if intento == numero_secreto:
-            print(f"¡Felicidades! Has adivinado el número en {intentos} intentos. ")
+            print(f"¡Felicidades! Has adivinado el número secreto {numero_secreto} en {intentos} intentos.")
             break
-        elif intento < numero_secreto:
-            print("El número secreto es mayor. Intenta de nuevo.")
         else:
-            print("El número secreto es menor. Intenta de nuevo.")
+            print(dar_pista(intento, numero_secreto))
     else:
-        print(f"Lo siento, has agotado tus {max_intentos} intentos. El número secreto era {numero_secreto}.")
-        jugar()
+        print(f"Se acabaron los intentos. El número secreto era {numero_secreto}. ¡Mejor suerte la próxima vez!")
 
 if __name__ == "__main__":
     jugar()
